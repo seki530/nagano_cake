b0b764f410eac68bd05ce4db212725f1470c7661
EC2 Default User <ec2-user@ip-172-31-23-167.ap-northeast-1.compute.internal>
2023-07-11 11:32:59 +0000

    adminユーザ情報編集画面

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,25 +1,34 @@
 class Public::CartItemsController < ApplicationController
-  
+
   def index
   end
-  
-  
-  
+
+
+
   def create
+    @cart_item = CartItem.new(cart_item_params)
+    @cart_item.customer_id = current_customer.id
+    @cart_item.item_id = params[:item_id]
+    redirect_to cart_items_path, notice: 'Successfully added product to your cart'
   end
-  
-  
+
+
   def update
   end
-  
-  
+
+
   def destroy
   end
-  
-  
+
+
   def destroy_all
   end
-  
-  
-  
+
+
+
 end
+
+private
+  def cart_item_params
+      params.require(:cart_item).permit(:item_id, :amount)
+  end
--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -37,3 +37,4 @@ end
    def customer_params
        params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :email, :postal_code, :address, :telephone_number, :is_deleted)
    end
+   
