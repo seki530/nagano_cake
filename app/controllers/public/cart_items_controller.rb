0e027991b7926f42ac057c8dee92dbbaf5d01012
EC2 Default User <ec2-user@ip-172-31-23-167.ap-northeast-1.compute.internal>
2023-07-15 11:35:01 +0000

    カートに同一商品があった場合の処理の実装

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -10,9 +10,22 @@ class Public::CartItemsController < ApplicationController
   def create
     @cart_item = CartItem.new(cart_item_params)
     @cart_item.customer_id = current_customer.id
-    @cart_item.item_id = params[:item_id]
-    @cart_item.save
-    redirect_to cart_items_path
+    
+    if current_customer.cart_items.find_by(item_id: params[:item_id]).present?
+      @cart_item = current_customer.cart_items.find_by(item_id: params[:item_id])
+      @cart_item.amount += params[:amount].to_i
+      @cart_item.save
+      flash[:notice] = "商品の個数が変更されました！"
+      redirect_to cart_items_path
+      
+    elsif 
+      @cart_item.save
+      redirect_to cart_items_path
+      
+    else
+      redirect_to cart_items_path
+    end
+
   end
 
 
@@ -33,7 +46,7 @@ class Public::CartItemsController < ApplicationController
   def destroy_all
      @cart_item = current_customer.cart_items
      @cart_item.destroy_all
-     redirect_to cart_items_path
+     redirect_to destroy_all_path
   end
 
 
