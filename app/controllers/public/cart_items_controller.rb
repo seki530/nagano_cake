8b7c8364006a58f1b3912823de12ba8c53071d17
EC2 Default User <ec2-user@ip-172-31-23-167.ap-northeast-1.compute.internal>
2023-07-26 03:22:24 +0000

    最終調整,修正

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -11,11 +11,11 @@ class Public::CartItemsController < ApplicationController
     @cart_item = CartItem.new(cart_item_params)
     @cart_item.customer_id = current_customer.id
     @cart_item.item_id = cart_item_params[:item_id]
-  if current_customer.cart_items.find_by(item_id: params[:item_id]).present?
-      @cart_item = current_customer.cart_items.find_by(item_id: params[:item_id])
-      @cart_item.amount += params[:amount].to_i
+
+  if current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id]).present?
+      @cart_item = current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id])
+      @cart_item.amount += params[:cart_item][:amount].to_i
       @cart_item.update(amount: @cart_item.amount)
-      @cart_item.save
       redirect_to cart_items_path
 
   else
@@ -28,7 +28,7 @@ class Public::CartItemsController < ApplicationController
 
   def update
     cart_item = CartItem.find(params[:id])
-    cart_item.update!(cart_item_params)
+    cart_item.update(cart_item_params)
      redirect_to cart_items_path(@cart_item)
   end
 
